8756e3dc5c7a8675f6493e5eecadc227a069f303
AKULA GEETHIKA SAI SRIJA <a.geethika1507@gmail.com>
2026-05-18 11:35:13 +0530

    Create 8. SecurityConfig.java

---
+++ 8. SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.codealpha.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic();
+
+        return http.build();
+    }
+}
